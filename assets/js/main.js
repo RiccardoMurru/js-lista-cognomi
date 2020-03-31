a0ab6d63e43bfd842a686a392a42a6cd971ae143
Riccardo Murru <riccardo.mr@gmail.com>
2020-03-31 15:43:29 +0200

    add. enter surname and print alphabetical list

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+// 1. Chiedere all'utente il cognome
+// 2. Inserire il cognome in un array
+// 3. Stampare la lista ordinata alfabeticamente
+// 4. Scrivere la posizione nella lista in cui si trova il nuovo utente
+
+
+
+// Variabili
+var users = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
+var surname = document.getElementById('surname');
+var btnEnter = document.getElementById('btn-enter');
+var btnGenerateList = document.getElementById('btn-genera-lista');
+var listElement = document.getElementById('user-list');
+var position = document.getElementById('position');
+
+
+// 1-2. Chiedere cognome e inserire nell'array
+
+btnEnter.addEventListener('click',
+    function() {
+        users.push(surname.value);
+        surname.value = '';
+    }
+);
+
+// 3. Stampare la lista in ordine alfabetico
+
+btnGenerateList.addEventListener('click',
+    function() {
+        for (var i = 0; i < users.length; i++) {
+            var item = users.sort()[i];
+            listElement.innerHTML +=  '<li>' + item + '</li>';
+        }
+        
+    }
+);
+
+// 4. Scrivere in che posizione si trova il nuovo utente
+
